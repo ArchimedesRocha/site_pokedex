06b55119ae270bb57241825ec7ed9c61c2bb7055
Archimedes Rocha <dev.archimedesrocha@gmail.com>
2023-02-06 16:53:40 -0300

    Adjust Layout

--- src/front-end/assets/js/lib/modal-card-pokemon.js
+++ src/front-end/assets/js/lib/modal-card-pokemon.js
@@ -1,18 +1,38 @@
-const openCardPokemon = document.querySelectorAll('.js-open-details-pokemon')
-const closeCardPokemon = document.querySelectorAll('.js-close-details-pokemon')
+let activeModal = null;
 
-function openDetailsPokemon() {
-  document.documentElement.classList.add('open-modal')
+const selectCard = document.querySelectorAll('.js-open-details-pokemon');
+const closeCardPokemon = document.querySelectorAll('.js-close-details-pokemon');
+const openModal = document.querySelectorAll('.s-modal');
+
+function handleCardClick(event) {
+  event.preventDefault();
+  
+  // Remove a classe "active" dos cartões selecionados
+  selectCard.forEach((card) => card.classList.remove("active"));
+  
+  // Adiciona a classe "active" ao cartão clicado
+  event.currentTarget.classList.add("active");
+  
+  // Remove a classe "active-modal" do modal ativo, se houver
+  if (activeModal) {
+    activeModal.classList.remove("active-modal");
+  }
+  
+  // Adiciona a classe "active-modal" ao modal correspondente ao cartão clicado
+  activeModal = openModal[event.currentTarget.dataset.index];
+  activeModal.classList.add("active-modal");
 }
 
-function closeDetailsPokemon() {
-  document.documentElement.classList.remove('open-modal')
+function handleCloseClick() {
+  activeModal.classList.remove('active-modal');
+  activeModal = null;
 }
 
-openCardPokemon.forEach(card => {
-  card.addEventListener('click', openDetailsPokemon)
-})
+selectCard.forEach((card, index) => {
+  card.dataset.index = index;
+  card.addEventListener("click", handleCardClick);
+});
 
-closeCardPokemon.forEach(card => {
-  card.addEventListener('click', closeDetailsPokemon)
-})
+closeCardPokemon.forEach((closeBtn) => {
+  closeBtn.addEventListener('click', handleCloseClick);
+});
